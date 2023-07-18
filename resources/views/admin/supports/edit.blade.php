c5a5ca99f84bb27390cbdfa8487fbab92900906b
Yuri Santos de Lima <yuri.y.u.r.i@hotmail.com>
2023-07-18 08:35:25 -0300

    feat: improve views with include and components

--- resources/views/admin/supports/edit.blade.php
+++ resources/views/admin/supports/edit.blade.php
@@ -7,9 +7,6 @@
 @endif
 
 <form action="{{ route('supports.update', $support->id) }}" method="POST">
-    @csrf()
     @method('PUT')
-    <input type="text" name="subject" placeholder="assunto" value="{{ $support->subject }}">
-    <textarea name="body" cols="30" rows="10">{{ $support->body }}</textarea>
-    <button type="submit">Enviar</button>
+    @include('admin.supports.partials.form', ['support' => $support])
 </form>
